debe0031d746ee6255d8c785bb66a5252896dd43
David Cerbino <david.cerbino@ing.austra.edu.ar>
2016-06-03 18:44:12 -0300

    Scripts first try

--- Assets/script/Pipe.cs
+++ Assets/script/Pipe.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Pipe : MonoBehaviour {
 
+	private GameObject up, down, left, right; 
+
 	// Use this for initialization
 	void Start () {
 	
@@ -14,6 +16,21 @@ public class Pipe : MonoBehaviour {
 	}
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "pipe") {
+			Vector3 myDirection = this.gameObject.transform.forward;
+			Vector3 otherDirection = other.gameObject.transform.forward;
+
+			float angle = Vector3.Angle(otherDirection, myDirection);
+
+			if (angle < 90) {
+				Debug.Log ("up");	
+			} else if (angle >= 90) {
+				Debug.Log ("right");	
+			} else if (angle > 180) {
+				Debug.Log ("left");	
+			} else if (angle > 270) {
+				Debug.Log ("down");	
+			}
+
 			Debug.Log ("Collision");
 		}
 
