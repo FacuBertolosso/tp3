54e2a61bb784ab0b6dfa10abb6e8bc8d97859aa9
David Cerbino <david.cerbino@ing.austra.edu.ar>
2016-06-03 17:52:41 -0300

    added some script and colliders to marker

---
+++ Assets/script/GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public float graceTime;
+	public float fillTimeSec;
+	public float TrainspottingFrequency;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/script/Pipe.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pipe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "pipe") {
+			Debug.Log ("Collision");
+		}
+
+		//Destroy(other.gameObject);
+	}
+}
